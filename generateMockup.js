4ce8763f765efdf685c3437f33900e510902a676
daengdaenglee <gunho1020@gmail.com>
2018-08-25 23:52:30 +0900

    ENH: Generate mockup data

---
+++ generateMockup.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+
+const MAX_X = 100;
+const SERIES_NUM = 5;
+
+const csv = [
+  ['X', ...[...Array(SERIES_NUM)].map((v, i) => `Y_${i + 1}`)],
+  ...[...Array(MAX_X)].map((v1, i) => [
+    i + 1,
+    ...[...Array(SERIES_NUM)].map(() => Math.random() * 10),
+  ]),
+]
+  .map(row => row.join(','))
+  .join('\n');
+
+const steps = [...Array(10)].map((v, i) => ({
+  value: (i + 1) * Math.floor(MAX_X / 12),
+  label: `S_${i + 1}`,
+}));
+
+const data = {
+  data: csv,
+  steps,
+};
+
+// eslint-disable-next-line no-console
+fs.writeFile('sample.json', JSON.stringify(data), 'utf-8', e => console.log(e || 'Complete'));
--- index.js
+++ index.js
@@ -1,13 +1,11 @@
 import * as d3 from 'd3';
 
-window.onload = () => {
-  d3.select('svg')
-    .selectAll('text.sample')
-    .data(['Hello', 'World'])
-    .enter()
-    .append('text')
-    .attr('class', 'sample')
-    .attr('x', (d, i) => i * 100 + 20)
-    .attr('y', 100)
-    .text(d => d);
-};
+d3.select('svg')
+  .selectAll('text.sample')
+  .data(['Hello', 'World'])
+  .enter()
+  .append('text')
+  .attr('class', 'sample')
+  .attr('x', (d, i) => i * 100 + 20)
+  .attr('y', 100)
+  .text(d => d);
